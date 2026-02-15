b6770295219e46883eec9f933c81ca56c4e64443
barshatheprogrammer <barsham734.com>
2026-02-15 21:44:09 +0530

    up photo

--- client/src/data/works.ts
+++ client/src/data/works.ts
@@ -3,26 +3,26 @@ import type { IWork } from "../types";
 export const works: IWork[] = [
     {
         title: "Smart Bin Prototype",
-        image: "/public/smart.png",
+        image: "/smart.png",
     },
     {
         title: "Mobile App Interface",
-        image: "/public/app.png",
+        image: "/app.png",
     },
     {
         title: "Community Cleanup Event",
-        image: "/public/community.png",
+        image: "/community.png",
     },
     {
         title: "Recycling Center Partnership",
-        image: "/public/recycle.png",
+        image: "/recycle.png",
     },
     {
         title: "Educational Workshop",
-        image: "/public/workshop.png",
+        image: "/workshop.png",
     },
     {
         title: "New Zero-Waste Initiative",
-        image: "/public/zero.png",
+        image: "/zero.png",
     },
 ];
