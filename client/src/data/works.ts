65035c420c40a5dd424094f7633abdbbb07a2fde
barshatheprogrammer <barsham734.com>
2026-02-15 21:30:55 +0530

    photo change

--- client/src/data/works.ts
+++ client/src/data/works.ts
@@ -3,26 +3,26 @@ import type { IWork } from "../types";
 export const works: IWork[] = [
     {
         title: "Smart Bin Prototype",
-        image: "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=600&auto=format&fit=crop",
+        image: "/public/smart.png",
     },
     {
         title: "Mobile App Interface",
-        image: "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?q=80&w=600&auto=format&fit=crop",
+        image: "/public/app.png",
     },
     {
         title: "Community Cleanup Event",
-        image: "https://images.unsplash.com/photo-1618477461853-cf6ed80faba5?q=80&w=600&auto=format&fit=crop",
+        image: "/public/community.png",
     },
     {
         title: "Recycling Center Partnership",
-        image: "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=600&auto=format&fit=crop",
+        image: "/public/recycle.png",
     },
     {
         title: "Educational Workshop",
-        image: "https://images.unsplash.com/photo-1544531586-fde5298cdd40?q=80&w=600&auto=format&fit=crop",
+        image: "/public/workshop.png",
     },
     {
         title: "New Zero-Waste Initiative",
-        image: "https://images.unsplash.com/photo-1595278069441-2cf29f8005a4?q=80&w=600&auto=format&fit=crop",
+        image: "/public/zero.png",
     },
 ];
